6be94422d6a47e23df5334b5a816d635f7a67d72
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2026-05-22 15:57:25 +0100

    feat(nrf): initial implementation of nrf

---
+++ services/nrf/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import yaml
+
+with open("nrf.conf.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+app = FastAPI()
+
+
+@app.get("/regions/{region_id}")
+def read_region(region_id: str, serviceType: str | None = None):
+    region = config["regions"].get(region_id)
+    if region is None:
+        return {"error": "Region not found"}
+    if serviceType:
+        region = region.get(serviceType)
+        if region is None:
+            return {"error": "Service type not found"}
+    return region
